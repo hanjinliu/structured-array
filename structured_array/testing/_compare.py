2c84b591353b3725463e6003cae52d5be79f9a83
Hanjin Liu <liuhanjin-sc@i.softbank.jp>
2024-10-13 22:56:22 +0900

    fix coverage

--- structured_array/testing/_compare.py
+++ structured_array/testing/_compare.py
@@ -5,12 +5,10 @@ import structured_array as st
 
 
 def assert_array_equal(a: st.StructuredArray, b: st.StructuredArray):
-    if not isinstance(a, st.StructuredArray):
-        a = st.array(a)
-    if not isinstance(b, st.StructuredArray):
-        b = st.array(b)
-    if a.columns != b.columns:
+    assert isinstance(a, st.StructuredArray)
+    assert isinstance(b, st.StructuredArray)
+    if a.columns != b.columns:  # pragma: no cover
         raise AssertionError(f"Columns do not match.\na: {a.columns}\nb: {b.columns}")
-    if a.dtypes != b.dtypes:
+    if a.dtypes != b.dtypes:  # pragma: no cover
         raise AssertionError(f"Data types do not match.\na: {a.dtypes}\nb: {b.dtypes}")
     np_testing.assert_array_equal(a._arr, b._arr)
